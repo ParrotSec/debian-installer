4fb7ebac30d1849ac86641fd5b1efd7d82cefa89
Lorenzo Palinuro Faletra <palinuro@parrotsec.org>
2017-02-13 17:57:28 +0100

    Import Debian version 20170127+parrot0
    debian-installer (20170127+parrot0) parrot; urgency=medium
    
      * Import new Debian release
    
    debian-installer (20170127) unstable; urgency=medium
    
      [ Cyril Brulebois ]
      * Bump Linux kernel version from 4.8.0-2 to 4.9.0-1.
      * Update translation-status for the release.
    
      [ Martin Michlmayr ]
      * Drop armel/versatile flavour since kernel support was removed.
    
      [ Aurelien Jarno ]
      * Also copy *.so files in udeblibs (Closes: #851790).
      * mips*: install all NIC modules in the netbood initrd.
    
    debian-installer (20170112) unstable; urgency=medium
    
      [ Cyril Brulebois ]
      * Bump Linux kernel version from 4.7.0-1 to 4.8.0-1.
      * Add “set -e;” before while loops in a few arm* config files
        to make sure errors are reported (Closes: #843916).
      * Revert the following workaround after changes in grub2 2.02~beta3:
        config/kfreebsd.cfg: Put xorriso's -J flag after “--” to work around
        grub-mkrescue's change in option handling (See: #741656).
      * Exclude libssl1.1-udeb as well for qnap, wget will likely depend on
        it later on.
      * Update translation-status for the release.
    
      [ Adam Conrad ]
      * build/util/efi-image: Fix up FAT filesystem size calculation:
        - We appear to need 32k headroom, not 24k; this was masked
          by rounding errors and blind luck until grub grew 1.5kB.
        - Break out the magic headroom number into 32+31 to match
          the comment and make the arithmetic more obvious to read.
        - Round file sizes up, not down, so we don't break in future.
    
      [ Martin Michlmayr ]
      * Disable u-boot images for OpenRD because they've been removed
        (see #837629).
      * Exclude wpasupplicant-udeb on orion5x-qnap because it brings in
        libcrypto1.1-udeb.
    
      [ Aurélien COUDERC ]
      * Update theme to Soft Waves by Juliette Belin.
    
      [ Christian Perrier ]
      * Replace fonts-lklug-sinhala by fonts-noto-hinted-udeb for Sinhala.
        Closes: #837926
    
      [ Cyril Brulebois ]
      * Bump Linux kernel version from 4.8.0-1 to 4.8.0-2.
    
      [ Philipp Kern ]
      * Add https support to the installer. (Closes: #842040)
    
      [ Philip Hands ]
      * pkg-lists/gtk-common: ttf-dejavu-udeb is now called fonts-dejavu-udeb
    
      [ Samuel Thibault ]
      * Bump gnumach kernel version.

--- build/util/arm/ss4000e/up.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo "Writing zImage to flash:" > /sysroot/var/log/upgrade.log
-/fs/writeflash -z /sysroot/zImage > /sysroot/var/log/upgrade.log 2>&1
-echo "Writing initrd to flash:" > /sysroot/var/log/upgrade.log
-/fs/writeflash -r /sysroot/initrd.gz > /sysroot/var/log/upgrade.log 2>&1
